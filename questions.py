872a513a1e18b128a91ee4327eaf235b56b53a02
Delfina Flaherty <you@example.com>
2025-03-15 10:40:29 -0300

    Junté las listas para no ingresar con índices (Inciso 3)

--- questions.py
+++ questions.py
@@ -26,14 +26,14 @@ correct_answers_index = [1, 2, 0, 3, 1]
 #Inicializo el puntaje en cero
 score = 0 
 
-# El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions)-1)
+#Modifico y junto las 3 listas
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k = 3)
 
-    # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+# El usuario deberá contestar 3 preguntas
+for questions, possible_answers, correct_index in questions_to_ask:
+    #Muestra la pregunta y las respuestas
+    print(questions)
+    for i, answer in enumerate(possible_answers):
         print(f"{i + 1}. {answer}")
 
     # El usuario tiene 2 intentos para responder correctamente
@@ -42,11 +42,11 @@ for _ in range(3):
         if user_answer.isdigit(): 
           user_answer = int(user_answer) -1
           #Verifico si el número está dentro del rango de respuestas
-          if user_answer < 0 or user_answer >= len(answers[question_index]):
+          if user_answer < 0 or user_answer >= len(possible_answers):
             print("Respuesta no válida")
             break
           # Se verifica si la respuesta es correcta
-          if user_answer == correct_answers_index[question_index]:
+          if user_answer == correct_index:
             print("¡Correcto!")
             #Sumo un punto si es correcta
             score += 1
@@ -55,7 +55,7 @@ for _ in range(3):
             # Si el usuario no responde correctamente después de 2 intentos,
             # se muestra la respuesta correcta
             print("Incorrecto. La respuesta correcta es:")
-            print(answers[question_index] [correct_answers_index[question_index]])
+            print(possible_answers[correct_index])
             score -=0.5
             break
         else:
